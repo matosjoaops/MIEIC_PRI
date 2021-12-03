2b56c4a81638bec4c976e22707e6741d181c9d3d
MechJM <matosjoaops@protonmail.com>
2021-12-03 11:58:53 +0000

    Finished tutorial 6

--- Tutoriais/06-evaluation/script.py
+++ Tutoriais/06-evaluation/script.py
@@ -1,3 +1,4 @@
+### PART 1
 # SETUP
 import matplotlib.pyplot as plt
 from sklearn.metrics import PrecisionRecallDisplay
@@ -5,10 +6,11 @@ import numpy as np
 import json
 import requests
 import pandas as pd
+import sys
 
 QRELS_FILE = "information_systems_qrels.txt"
 QUERY_URL = "http://localhost:9200/courses/_search"
-QUERY_FILE = "query2.json"
+QUERY_FILE = sys.argv[1]
 
 query_json = json.load(open(QUERY_FILE))
 
@@ -18,3 +20,86 @@ relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
 
 results = requests.get(QUERY_URL, json=query_json).json()['hits']['hits']
 
+### PART 2
+
+# METRICS TABLE
+# Define custom decorator to automatically calculate metric based on key
+metrics = {}
+metric = lambda f: metrics.setdefault(f.__name__, f)
+
+@metric
+def ap(results, relevant):
+    """Average Precision"""
+    precision_values = [
+        len([
+            doc 
+            for doc in results[:idx]
+            if doc['_source']['id'] in relevant
+        ]) / idx 
+        for idx in range(1, len(results))
+    ]
+    return sum(precision_values)/len(precision_values)
+
+@metric
+def p10(results, relevant, n=10):
+    """Precision at N"""
+    return len([doc for doc in results[:n] if doc['_source']['id'] in relevant])/n
+
+def calculate_metric(key, results, relevant):
+    return metrics[key](results, relevant)
+
+# Define metrics to be calculated
+evaluation_metrics = {
+    'ap': 'Average Precision',
+    'p10': 'Precision at 10 (P@10)'
+}
+
+# Calculate all metrics and export results as LaTeX table
+df = pd.DataFrame([['Metric','Value']] +
+    [
+        [evaluation_metrics[m], calculate_metric(m, results, relevant)]
+        for m in evaluation_metrics
+    ]
+)
+
+with open('results.tex','w') as tf:
+    tf.write(df.to_latex())
+
+### PART 3
+
+# PRECISION-RECALL CURVE
+# Calculate precision and recall values as we move down the ranked list
+precision_values = [
+    len([
+        doc 
+        for doc in results[:idx]
+        if doc['_source']['id'] in relevant
+    ]) / idx 
+    for idx, _ in enumerate(results, start=1)
+]
+
+recall_values = [
+    len([
+        doc for doc in results[:idx]
+        if doc['_source']['id'] in relevant
+    ]) / len(relevant)
+    for idx, _ in enumerate(results, start=1)
+]
+
+precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
+
+# Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
+recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
+recall_values = sorted(set(recall_values))
+
+# Extend matching dict to include these new intermediate steps
+for idx, step in enumerate(recall_values):
+    if step not in precision_recall_match:
+        if recall_values[idx-1] in precision_recall_match:
+            precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
+        else:
+            precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
+
+disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
+disp.plot()
+plt.savefig('precision_recall.pdf')
